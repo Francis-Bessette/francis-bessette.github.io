ec30f5861348c16b153e3eb3e97d6de18929d5c4
Francis Bessette <127312249+Francis-Bessette@users.noreply.github.com>
2023-03-23 12:51:47 -0400

    Update and rename contactSub.php to form-to-email.php

---
+++ form-to-email.php
@@ -0,0 +1,21 @@
+<?php
+if(lisset($_POST['submit']))
+{
+  echo "error; you need to submit the form";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+if(empty($name)||empty($visitor_email))
+{
+  echo "Name and email are mandatory";
+  exit;
+}
+
+$email_form = 'francisbessette07@gmail.com';
+$email_subject = 'New Contact Submisson';
+$email_body = 'Name: $name \n Email: $visitor_email \n Message: $message';
+$to = 'francisbessette07@gmail.com';
+$headers = "From: $email_from \r\n";
+mail($to,$email_subject,$email_body,$headers);
